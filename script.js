420c69e7a1b6782eb8eeb712a7819c6decc65a67
Jakub Molinski <jakubmolinski9@gmail.com>
2024-03-24 12:21:28 +0100

    Created a javascript function that generates given amount of squares

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const popupBtn = document.querySelector(".popup-btn");
-const container = document.querySelector(".container");
+
+const createGridLayout = (numberOfSquares) => {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+};
+
+createGridLayout(16);
